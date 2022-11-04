323ca412e0e64a8c804653629398cd3b3f265f39
ivansurzhikov29 <114010595+ivansurzhikov29@users.noreply.github.com>
2022-11-04 20:41:19 +0300

    Add files via upload

---
+++ практос7,2упр.py
@@ -0,0 +1,16 @@
+x = []
+for i in range(15):
+	x.append(int(input()))
+y = sum(x)/len(x)
+print("среднеe aрифмитическое массива х =", y)
+z = []
+for i in range(10):
+	z.append(int(input()))
+h = sum(z)/len(z)
+print("среднеe aрифмитическое массива h =", h)
+l = []
+for i in range(5):
+	 l.append(int(input()))
+j = sum(l)/len(l)
+print("среднеe aрифмитическое массива j =", j)
+	
